592bb2927bae9d91ee3145bc18b4f374a0201040
unknown <mariah@example.com>
2025-09-02 11:05:46 -0300

    exercicio

--- exercicio1.py
+++ exercicio1.py
@@ -4,9 +4,10 @@ if num > 5:
 elif num <=5:
     print("menor ou igual a 5")
 
-    num = 8
-    if num ==7:
-        print("igual a 7")
-    else:
-        print("diferemte de 7")
+num = 8
+if num ==7
+    print("igual a 7")
+else:
+    print("diferemte de 7")
+
         
---
+++ exercicio2.py
@@ -0,0 +1,5 @@
+num = 8
+if num ==7
+    print("igual a 7")
+else:
+    print("diferemte de 7")
