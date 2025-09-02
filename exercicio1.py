351546d38c51c181dd270dd4cb41a05b9af2420f
unknown <mariah@example.com>
2025-09-02 08:34:17 -0300

    teste

--- exercicio1.py
+++ exercicio1.py
@@ -1,1 +1,12 @@
-print("olá")
+num = 10
+if num > 5:
+    print("maior que 5")
+elif num <=5:
+    print("menor ou igual a 5")
+
+    num = 8
+    if num ==7:
+        print("igual a 7")
+    else:
+        print("diferemte de 7")
+        
